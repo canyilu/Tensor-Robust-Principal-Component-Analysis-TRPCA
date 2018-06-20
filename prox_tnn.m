272322e53ee65cb06d5b842a95ad03ce9fdf9a23
Canyi Lu <canyilu@gmail.com>
2018-06-20 00:16:54 -0400

    Add files via upload

--- prox_tnn.m
+++ prox_tnn.m
@@ -25,7 +25,6 @@ function [X,tnn,trank] = prox_tnn(Y,rho)
 %
 
 [n1,n2,n3] = size(Y);
-max12 = max(n1,n2);
 X = zeros(n1,n2,n3);
 Y = fft(Y,[],3);
 tnn = 0;
@@ -34,27 +33,26 @@ trank = 0;
 % first frontal slice
 [U,S,V] = svd(Y(:,:,1),'econ');
 S = diag(S);
-S = max(S-rho,0);
-tol = max12*eps(max(S));
-r = sum(S > tol);
-S = S(1:r);
-X(:,:,1) = U(:,1:r)*diag(S)*V(:,1:r)';
-tnn = tnn+sum(S);
-trank = max(trank,r);
-
+r = length(find(S>rho));
+if r>=1
+    S = S(1:r)-rho;
+    X(:,:,1) = U(:,1:r)*diag(S)*V(:,1:r)';
+    tnn = tnn+sum(S);
+    trank = max(trank,r);
+end
 % i=2,...,halfn3
 halfn3 = round(n3/2);
 for i = 2 : halfn3
     [U,S,V] = svd(Y(:,:,i),'econ');
     S = diag(S);
-    S = max(S-rho,0);
-    tol = max12*eps(max(S));
-    r = sum(S > tol);
-    S = S(1:r);
-    X(:,:,i) = U(:,1:r)*diag(S)*V(:,1:r)';    
+    r = length(find(S>rho));
+    if r>=1
+        S = S(1:r)-rho;
+        X(:,:,i) = U(:,1:r)*diag(S)*V(:,1:r)';
+        tnn = tnn+sum(S)*2;
+        trank = max(trank,r);
+    end
     X(:,:,n3+2-i) = conj(X(:,:,i));
-    tnn = tnn+sum(S)*2;
-    trank = max(trank,r);
 end
 
 % if n3 is even
@@ -62,13 +60,13 @@ if mod(n3,2) == 0
     i = halfn3+1;
     [U,S,V] = svd(Y(:,:,i),'econ');
     S = diag(S);
-    S = max(S-rho,0);
-    tol = max12*eps(max(S));
-    r = sum(S > tol);
-    S = S(1:r);
-    X(:,:,i) = U(:,1:r)*diag(S)*V(:,1:r)';
-    tnn = tnn+sum(S);
-    trank = max(trank,r);
+    r = length(find(S>rho));
+    if r>=1
+        S = S(1:r)-rho;
+        X(:,:,i) = U(:,1:r)*diag(S)*V(:,1:r)';
+        tnn = tnn+sum(S);
+        trank = max(trank,r);
+    end
 end
 tnn = tnn/n3;
 X = ifft(X,[],3);
